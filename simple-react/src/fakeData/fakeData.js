b4b71383d69371742b096c49d6119b98c5dddb3e
ratnakarmaker <73543192+ratnakarmaker@users.noreply.github.com>
2021-03-01 13:26:14 +0600

    data

---
+++ simple-react/src/fakeData/fakeData.js
@@ -0,0 +1,62 @@
+const femaleCricketTeam =  [{
+    "id": 1,
+    "Name": "Maison Peachey",
+    "Salary": "$5.99",
+    "Team Name": "transition granular e-tailers",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWRqnwqGwTbJxgUJY6vREEFAA81J879vCDEA&usqp=CAU"
+  }, {
+    "id": 2,
+    "Name": "Valerye Headingham",
+    "Salary": "$1.93",
+    "Team Name": "iterate one-to-one bandwidth",
+    "Image": "2wCEAAkGBxMSEBUQEBIWDxUPFRUWFhUPFRAPFRUVFRUWFhUVFRUYHSggGB0lGxUVITEhJSkrLi4uFx8zODMtNygtLi0BCgoKDg0OFxAQGi0lHyUvLSstKy0tLS0tLS0tLS0tLSsvLS0tLS0tLS0rLS0tLSstLS0tLS0tLS0tLS0tLS0tK"
+  }, {
+    "id": 3,
+    "Name": "Montague Dubock",
+    "Salary": "$8.37",
+    "Team Name": "optimize extensible content",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwPRnC2wl8N9S0bspTl30aPzN3r3GVGuwSPw&usqp=CAU"
+  }, {
+    "id": 4,
+    "Name": "Khalil Vyse",
+    "Salary": "$0.50",
+    "Team Name": "productize wireless initiatives",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMhKAQJ9SwV1TzZb-sQyBMrzk3ye3a3cZoVw&usqp=CAU"
+  }, {
+    "id": 5,
+    "Name": "Phillis Freshwater",
+    "Salary": "$8.89",
+    "Team Name": "reinvent scalable mindshare",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrN0r3UVYsevTE_V7uldcRLy3c6MtI_-53Q&usqp=CAU"
+  }, {
+    "id": 6,
+    "Name": "Linnell Churching",
+    "Salary": "$6.43",
+    "Team Name": "implement next-generation users",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO4Tc6aoBKqYlNjENtL5hSdDY6nrNfsrFxRQ&usqp=CAU"
+  }, {
+    "id": 7,
+    "Name": "Sherie O'Mannion",
+    "Salary": "$0.11",
+    "Team Name": "evolve plug-and-play eyeballs",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMdfNwVaWBWLx_OuIjsCRRCJhiFu0WAbDwJA&usqp=CAU"
+  }, {
+    "id": 8,
+    "Name": "Hayley Hedge",
+    "Salary": "$4.69",
+    "Team Name": "innovate leading-edge e-commerce",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfpCJ3YV6t8HnsKg9AyGXeav2vR1HlWUJ9Yg&usqp=CAU"
+  }, {
+    "id": 9,
+    "Name": "Broderic Allenby",
+    "Salary": "$8.62",
+    "Team Name": "engage rich deliverables",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShqU2vqjJnasBqW337Avnb5ndbq95X_lDGFQ&usqp=CAU"
+  }, {
+    "id": 10,
+    "Name": "Bethena Hamlin",
+    "Salary": "$7.44",
+    "Team Name": "transition holistic platforms",
+    "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKfgW8sBOIoVBlCzmsy_ydzM09ed0FK8tH2A&usqp=CAU"
+  }]
+  export default femaleCricketTeam;
